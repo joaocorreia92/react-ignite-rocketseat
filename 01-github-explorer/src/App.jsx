d9718fe45fdf2c5104199910aeb7093ada6cc312
Joao Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-03-17 01:27:02 -0300

    finished

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,3 +1,10 @@
+import { RepositoryList } from './components/repositoryList'
+import './styles/global.scss'
+
 export function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <RepositoryList />
+    </>
+  )
 }
