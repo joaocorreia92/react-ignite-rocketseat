ceece80d02f8e82ee5d9aba98c1038e75deaafed
Joao Pedro Correia Barros <jpedrocbarros@gmail.com>
2021-03-18 13:33:38 -0300

    finished

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,16 +2,21 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
-import { useState } from "react"
 
-//https://api/github.com/orgs/rocketseat/repos
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+//https://api.github.com/orgs/rocketseat/repos
 
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
-    fetch('https://api/github.com/orgs/rocketseat/repos')
+    fetch('https://api.github.com/orgs/rocketseat/repos')
       .then(response => response.json())
       .then(data => setRepositories(data))
   }, []);
